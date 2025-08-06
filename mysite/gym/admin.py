5807b6e30901fd1c6bef0096f4756d52ae13ebbd
S-e-r-g-io <126773536+S-e-r-g-io@users.noreply.github.com>
2025-08-06 16:38:49 +0200

    Imported db data except FasciaOraria

--- mysite/gym/admin.py
+++ mysite/gym/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Cliente, Abbonamento, Sala, Posto, Prenotazione, SubAbbonamento, FasciaOraria
 # Register your models here.
+
+admin.site.register(Cliente)
+admin.site.register(Abbonamento)
+admin.site.register(Sala)
+admin.site.register(Posto)
+admin.site.register(Prenotazione)
+admin.site.register(SubAbbonamento)
+admin.site.register(FasciaOraria)
