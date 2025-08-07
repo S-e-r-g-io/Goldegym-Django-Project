cbc2e4b549572a4597d4b6efeea22c969a2d6c5d
S-e-r-g-io <126773536+S-e-r-g-io@users.noreply.github.com>
2025-08-07 11:06:27 +0200

    added template logic for sala

--- mysite/gym/views.py
+++ mysite/gym/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Cliente, Abbonamento, Sala
 # Create your views here.
 
 def sala_view(request, *args, **kwargs):
-    my_context = {}
+    queryset = Sala.objects.all()
+    my_context = {
+        "list" : queryset
+    }
     return render(request,"sala.html", my_context)
